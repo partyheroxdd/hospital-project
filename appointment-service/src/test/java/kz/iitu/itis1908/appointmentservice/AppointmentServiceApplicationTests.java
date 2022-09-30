0eff2272fce87e53a8c09790f9031f53ee82124b
Miras Madiyev <miras.madiev15@gmail.com>
2022-09-30 18:02:35 +0600

    adding appointment module

---
+++ appointment-service/src/test/java/kz/iitu/itis1908/appointmentservice/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.itis1908.appointmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
