7c1e3f03deca48739d650d214e7bbd003dbf5f10
Miras Madiyev <miras.madiev15@gmail.com>
2022-09-30 17:06:29 +0600

    first commit

---
+++ doctor-service/src/main/java/kz/iitu/itis1908/doctorservice/DoctorServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.iitu.itis1908.doctorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DoctorServiceApplication.class, args);
+  }
+
+}
